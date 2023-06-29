8f86b43084117a19c0eecefb97ec9f982946d4c6
Crowdbotics <team@crowdbotics.com>
2023-06-29 14:04:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shopigram-41812",
+  url: "https://shopigram-41812.botics.co",
+  api: "https://shopigram-41812.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
